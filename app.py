ff6b86eb4dd7bb98bf3c92519d55aec38429e0d6
Alan Gladston <alangladston2004@gmail.com>
2024-04-28 01:54:55 +0530

    fixed the error in new.ipynb (i think lets see )

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask, render_template, redirect, url_for,request
 
 app = Flask(__name__)
 app.debug = True
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/predict")
+@app.route("/predict", methods=['POST','GET'])
+@app.route("/predict/", methods=['POST','GET'])
 def predict():
+    if request.method == 'POST':
+        # Retrieve form data
+        name = request.form['name']
+        age = request.form['age']
+        marital_status = request.form['maritalstatus']
+        work_type = request.form['Worktype']
+        residence = request.form['Residence']
+        gender = request.form['gender']
+        bmi = request.form['bmi']
+        gluc_level = request.form['gluclevel']
+        smoke = request.form['Smoke']
+        hypertension = request.form['Hypertension']
+        heart_disease = request.form['Heartdisease']
+
+        print(f"Name: {name}, Age: {age}, Marital Status: {marital_status}, Work Type: {work_type}, Residence: {residence}, Gender: {gender}, BMI: {bmi}, Glucose Level: {gluc_level}, Smoke: {smoke}, Hypertension: {hypertension}, Heart Disease: {heart_disease}")
     return render_template("predict.html")
 
-@app.route("/predict/")
+@app.route("/bmi")
 def bmi():
     return render_template("bmi.html")
 
